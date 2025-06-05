c6adc2e833b1721e82a74ccc2f1536b8765c0fe2
oboyle3 <po720999@sju.edu>
2025-06-04 21:52:04 -0400

    mock jira

--- config/settings.py
+++ config/settings.py
@@ -93,6 +93,7 @@ DATABASES = {
 }
 
 
+
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
--- enigmaapp/models.py
+++ enigmaapp/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Project(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    
+    owner = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='projects_owned'
+    )
+    
+    members = models.ManyToManyField(
+        User, related_name='projects_joined'
+    )
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
